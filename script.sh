4efa40eb7cce1a7d98c00b4af19d79919d2746ca
Kavindu Deshapriya <88641063+Kavindu-Deshapriya@users.noreply.github.com>
2025-03-18 15:02:34 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -12,6 +12,8 @@ sudo apt install -y \
     libarchive-tools gcc-aarch64-linux-gnu pigz python3 python2
 
 # Clone necessary repositories
+git clone --depth=1 https://github.com/Kavindu-Deshapriya/android_kernel_RMX3063Q.git
+cd android_kernel_RMX3063Q
 git clone --depth=1 https://github.com/techyminati/android_prebuilts_clang_host_linux-x86_clang-5484270 clang
 git clone --depth=1 https://github.com/EternalX-project/aarch64-linux-gnu.git gcc64
 git clone --depth=1 https://github.com/EternalX-project/arm-linux-gnueabi.git gcc32
